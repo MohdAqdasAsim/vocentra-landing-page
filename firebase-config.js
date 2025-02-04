2300d936e639bd992823d52156abaad817d100d1
Mohd Aqdas Asim <143924496+MohdAqdasAsim@users.noreply.github.com>
2025-02-04 12:07:18 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
-require('dotenv').config();
-
-const apiKey = process.env.API_KEY;
-const authDomain = process.env.FIREBASE_AUTH_DOMAIN;
-const projectId = process.env.FIREBASE_PROJECT_ID;
-const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
-const messagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
-const appId = process.env.FIREBASE_APP_ID;
+const apiKey = window.env.API_KEY;
+const authDomain = window.env.FIREBASE_AUTH_DOMAIN;
+const projectId = window.env.FIREBASE_PROJECT_ID;
+const storageBucket = window.env.FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = window.env.FIREBASE_MESSAGING_SENDER_ID;
+const appId = window.env.FIREBASE_APP_ID;
 
 const firebaseConfig = {
     apiKey: apiKey,
