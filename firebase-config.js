2c60e97d0102817190cffff2b6f57be377428eac
Mohd Aqdas Asim <143924496+MohdAqdasAsim@users.noreply.github.com>
2025-02-04 11:43:35 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
+require('dotenv').config();
+
+const apiKey = process.env.API_KEY;
+const authDomain = process.env.FIREBASE_AUTH_DOMAIN;
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
+const appId = process.env.FIREBASE_APP_ID;
+
 const firebaseConfig = {
-    apiKey: window.ENV.FIREBASE_API_KEY,
-    authDomain: window.ENV.FIREBASE_AUTH_DOMAIN,
-    projectId: window.ENV.FIREBASE_PROJECT_ID,
-    storageBucket: window.ENV.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: window.ENV.FIREBASE_MESSAGING_SENDER_ID,
-    appId: window.ENV.FIREBASE_APP_ID
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId
 };
 
 // Initialize Firebase
